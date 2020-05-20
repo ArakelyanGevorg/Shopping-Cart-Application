68d9dc3efc9c1de4e4a84875b378dfba8358d2c2
Gevorg <arakelyangevorg992@gmail.com>
2020-05-21 00:25:54 +0400

    first commit

---
+++ src/components/Copyright.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+export default class Copyright extends Component {
+    render() {
+        return (
+            <div>
+                <h4>IMPORTANT LINKS</h4>
+                <ul>
+                    <li>
+                        <a href="https://ru.reactjs.org/">
+                            Learn React JS
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+}
